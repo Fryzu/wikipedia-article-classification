b3782bb2a1b0402f435dd992d9e2ddc7fe11d325
Fryzu <bartek.fryz@gmail.com>
2021-06-02 17:44:10 +0200

    Adapts scrapper to new function

---
+++ TTI/TTI/TextDocument.py
@@ -0,0 +1,37 @@
+import requests
+
+BASE_URL = "https://en.wikipedia.org/w/api.php"
+
+
+def clean_document(document):
+    words = document.split()
+
+    cleaned_words = []
+    for word in words:
+        if len(word) > 1:
+            cleaned_words.append(word)
+
+    return " ".join(cleaned_words)
+
+
+def get_article_content(title):
+    response = requests.get(
+        url=BASE_URL,
+        params={
+            "action": "query",
+            "titles": title,
+            "prop": "extracts",  # get only the article content
+            "format": "json",
+            "explaintext": ""  # no html tags
+        }
+    )
+    if not (response.status_code < 400):
+        raise "Couldn't download article"
+
+    json = response.json()
+    content = list(json["query"]["pages"].values())[0]["extract"]
+    return clean_document(content)
+
+
+def extract_key_words(document):
+    pass
